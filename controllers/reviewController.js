99c3c27d5e00aa6354835ce6e1a507ce6f6634f3
hyeon-b <20201009@sungshin.ac.kr>
2024-05-08 15:18:03 +0900

    Feat: rating 평균 구하기 기능 추가

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -34,5 +34,40 @@ module.exports = {
       
     });
   },
-  
+
+  getAvgRate: async (req, res) =>{
+    let result={};
+    result.ra_regno = req.params.ra_regno;
+
+    const getOptions = {
+      host: "stop_bang_review_DB",
+      port: process.env.PORT,
+      path: `/db/review/findAllByRegno/${req.params.ra_regno}`,
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    httpRequest(getOptions).then((response) => {
+      const reviewCount = response.body.length;
+      
+      if (reviewCount>0){
+        let ratingSum = 0;
+
+        for (let review of response.body){
+          ratingSum += review.rating
+        }
+
+        result.avg = (ratingSum/reviewCount);
+    
+        return res.json(result);
+      }else{ 
+        result.avg = 0;
+        return res.json(result);
+      }
+
+    });
+
+  }
 }
+
